4aa4502346fe40eee85791649f0ec147a5e79af5
Dena-lab <denis.majrich@gmail.com>
2024-08-15 02:28:33 +0200

    Improved UI and added EEPROM functionality

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -105,16 +105,17 @@ void loop() {
           if (editMode) {
             lcd.setCursor(4, 0);
             if (currentLine == 0) {
-              Kp += 1;
+              Kp -= 1;
               lcd.print(Kp);
             } else if (currentLine == 1) {
-              Ki += 1;
+              Ki -= 1;
               lcd.print(Ki);
             } else if (currentLine == 2) {
-              Kd += 1;
+              Kd -= 1;
               lcd.print(Kd);
             } else if (currentLine == 3) {
               menuMode = false;
+              editMode = false;
               digitalWrite(buzzer, HIGH);
               delay(50);
               digitalWrite(buzzer, LOW);
@@ -133,21 +134,23 @@ void loop() {
           if (editMode) {
             lcd.setCursor(4, 0);
             if (currentLine == 0) {
-              Kp -= 1;
+              Kp += 1;
               lcd.print(Kp);
             } else if (currentLine == 1) {
-              Ki -= 1;
+              Ki += 1;
               lcd.print(Ki);
             } else if (currentLine == 2) {
-              Kd -= 1;
+              Kd += 1;
               lcd.print(Kd);
             } else if (currentLine == 3) {
               menuMode = false;
+              editMode = false;
               digitalWrite(buzzer, HIGH);
               delay(50);
               digitalWrite(buzzer, LOW);
               delay(50);
               lcd.clear();
+              lcd.cursor_off();
               lcd.setCursor(0, 0);
               lcd.print("Balance mode");
               lcd.setCursor(0, 1);
@@ -170,12 +173,36 @@ void loop() {
       if (editMode) {
         editMode = false;
         lcd.cursor_off();
-        lcd.setCursor(15, currentLine);
+        lcd.setCursor(15, 0);
         lcd.print(" ");
+        // Clear the EEPROM
+        for (int i = 0; i < 10; i++)
+        {
+          EEPROM.write(i, 0);
+          EEPROM.write(i + 10, 0);
+          EEPROM.write(i + 20, 0);
+        }
+        // Write the data to the EEPROM
+        KpStr = String(Kp);
+        KiStr = String(Ki);
+        KdStr = String(Kd);
+        for (unsigned int i = 0; i < KpStr.length(); i++)
+        {
+          EEPROM.write(i, KpStr[i]);
+        }
+        for (unsigned int i = 0; i < KiStr.length(); i++)
+        {
+          EEPROM.write(i + 10, KiStr[i]);
+        }
+        for (unsigned int i = 0; i < KdStr.length(); i++)
+        {
+          EEPROM.write(i + 20, KdStr[i]);
+        }
+        
       } else {
         editMode = true;
         lcd.cursor_on();
-        lcd.setCursor(15, currentLine);
+        lcd.setCursor(15, 0);
         lcd.print("*");
       }
 
@@ -185,7 +212,7 @@ void loop() {
       delay(50);
 
       while (digitalRead(encoderSwitch) == LOW) {}
-      delay(200);
+      delay(100);
     }
 
 
@@ -202,14 +229,20 @@ void balance() {
 
   if (digitalRead(encoderSwitch) == LOW) {
     menuMode = true;
+    editMode = false;
+    while (digitalRead(encoderSwitch) == LOW) {}
     lcd.cursor_on();
-    lcd.setCursor(15, 1);
-    lcd.print("E");
-    delay(500);
+    displayMenu();
     return;
   }
 
-  float setpoint = 0;
+  // PID variables
+  // The whole tray is 240mm long
+  // The servo can rotate 180 degrees
+  // But its limited to +-45 degrees
+  // The zero point is at 90 degrees
+  // The target distance is 100mm (acounting for the size of the ball)
+  float setpoint = 120;
   float error = 0;
   float lastError = 0;
   float integral = 0;
@@ -227,21 +260,14 @@ void balance() {
   duration = pulseIn(echo, HIGH);
   distance = (duration / 2) / 29.1;
 
-  if (distance < 10) {
-    digitalWrite(buzzer, HIGH);
-    delay(100);
-    digitalWrite(buzzer, LOW);
-    delay(100);
-  }
-
-  angle = map(encoderPos, -100, 100, 0, 180);
-  error = setpoint - angle;
+  error = setpoint - distance;
   integral += error;
   derivative = error - lastError;
   output = Kp * error + Ki * integral + Kd * derivative;
-  lastError = error;
 
-  servo.write(output);
+  angle = map(output, -100, 100, 45, 135);
+  servo.write(angle);
+
 
 }
 
@@ -254,35 +280,39 @@ void displayMenu() {
   switch (currentLine) {
     case 0:
       lcd.setCursor(0, 0);
-      lcd.print("Kp: ");
+      lcd.print("Kp:             ");
+      lcd.setCursor(4, 0);
       lcd.print(Kp);
       lcd.setCursor(0, 1);
-      lcd.print("Ki: ");
+      lcd.print("Ki:             ");
+      lcd.setCursor(4, 1);
       lcd.print(Ki);
       lcd.setCursor(4, 0);
       break;
     case 1:
       lcd.setCursor(0, 0);
-      lcd.print("Ki: ");
+      lcd.print("Ki:             ");
+      lcd.setCursor(4, 0);
       lcd.print(Ki);
       lcd.setCursor(0, 1);
-      lcd.print("Kd: ");
+      lcd.print("Kd:             ");
+      lcd.setCursor(4, 1);
       lcd.print(Kd);
       lcd.setCursor(4, 0);
       break;
     case 2:
       lcd.setCursor(0, 0);
-      lcd.print("Kd: ");
+      lcd.print("Kd:             ");
+      lcd.setCursor(4, 0);
       lcd.print(Kd);
       lcd.setCursor(0, 1);
-      lcd.print("Start? ");
-      lcd.print(" ");
+      lcd.print("Start?          ");
       lcd.setCursor(4, 0);
       break;
     case 3:
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Start? ");
+      lcd.print("Start?          ");
       lcd.print(" ");
       lcd.setCursor(6, 0);
       break;
